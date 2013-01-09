3bff06e45127b01f700a518628f2aa0cdd7175b6
Gasol Wu <gasolwu@kkbox.com>
2013-01-09 12:46:48 +0800

    Duck type

--- attachment/notify.py
+++ attachment/notify.py
@@ -1,4 +1,14 @@
-from trac.core import Component
+from trac.core import Component, implements
+from trac.attachment import IAttachmentChangeListener
 
 class AttachmentNotify(Component):
-    pass
+    implements(IAttachmentChangeListener)
+
+    def attachment_added(attachment):
+        pass
+
+    def attachment_deleted(attachment):
+        pass
+
+    def attachment_reparented(attachment, old_parent_realm, old_parent_id):
+        pass
